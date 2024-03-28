9eaea5da9330b565c5800948817b30f4d905da06
DEZester <aromudez@gmail.com>
2024-03-28 20:40:36 +0200

    Connect to db

--- cmd/mongodb/main.go
+++ cmd/mongodb/main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)                                //create object that gives API, access to MongoDB
-	opts := options.Client().ApplyURI(os.Getenv("MONGO_URI")).SetServerAPIOptions(serverAPI) //options for user for connect to DB
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Can't load .env file")
+		return
+	}
+	mongoURI := os.Getenv("MONGO_URI")
 
-	client, err := mongo.Connect(context.Background(), opts) //connect to DB
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)                  //create object that gives API, access to MongoDB
+	opts := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI) //options for user for connect to DB
+	client, err := mongo.Connect(context.Background(), opts)                   //connect to DB
 	if err != nil {
 		panic(err)
 	}
+
+	err = client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Database started successfully")
 }
