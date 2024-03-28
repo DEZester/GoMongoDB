eb1a208d2e8f0e981b62626573bbb68dddc6a45e
DEZester <aromudez@gmail.com>
2024-03-28 22:06:14 +0200

    Create handler.UpdateProductScockById

--- internal/handler/products.go
+++ internal/handler/products.go
@@ -55,6 +55,7 @@ func GetProductById(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
 	result := database.Products.FindOne(c, primitive.M{"_id": _id})
 	product := model.Product{}
 	err = result.Decode(&product)
@@ -68,7 +69,28 @@ func GetProductById(c *gin.Context) {
 }
 
 func UpdateProductScockById(c *gin.Context) {
+	id := c.Param(":id")
+	_id, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
+	var body struct {
+		Stock int `json:"stock" binding:"required"`
+	}
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, err = database.Products.UpdateOne(c, bson.M{"_id": _id}, bson.M{"$set": bson.M{"stock": body.Stock}})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "unable to update product"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, gin.H{"success": "product stock updated"})
 }
 
 func UpdateProductPricekById(c *gin.Context) {
