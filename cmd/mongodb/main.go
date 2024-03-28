a807cd21d6c2e563aaf60b87b84e93d6ffc31ceb
DEZester <aromudez@gmail.com>
2024-03-28 21:00:30 +0200

    Structure Files

--- cmd/mongodb/main.go
+++ cmd/mongodb/main.go
@@ -1,35 +1,30 @@
 package main
 
 import (
-	"context"
+	"GoMongoDB/internal/database"
 	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("Can't load .env file")
-		return
 	}
 	mongoURI := os.Getenv("MONGO_URI")
 
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)                  //create object that gives API, access to MongoDB
-	opts := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI) //options for user for connect to DB
-	client, err := mongo.Connect(context.Background(), opts)                   //connect to DB
-	if err != nil {
-		panic(err)
+	connectToDB := database.Init(mongoURI, "development")
+	if connectToDB != nil {
+		panic(connectToDB)
 	}
-	defer client.Disconnect(context.Background())
+	fmt.Println("Connected to MongoDB")
 
-	err = client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Database started successfully")
+	defer func() {
+		err := database.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
 }
---
+++ internal/database/mongo.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func Init(uri, database string) error {
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)             //create object that gives API, access to MongoDB
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI) //options for user for connect to DB
+
+	localClient, err := mongo.Connect(context.Background(), opts) //connect to DB
+	if err != nil {
+		return err
+
+	}
+
+	client = localClient
+
+	err = client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err()
+	return err
+}
+
+func Close() error {
+	return client.Disconnect(context.Background())
+}
