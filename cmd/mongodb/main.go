ce5cbc21c2b147ce224bde4acf2cedbf53df7887
DEZester <aromudez@gmail.com>
2024-03-28 20:26:15 +0200

    Add comments

--- cmd/mongodb/main.go
+++ cmd/mongodb/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
 	"os"
 
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(os.Getenv("MONGO_URI")).SetServerAPIOptions(serverAPI)
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)                                //create object that gives API, access to MongoDB
+	opts := options.Client().ApplyURI(os.Getenv("MONGO_URI")).SetServerAPIOptions(serverAPI) //options for user for connect to DB
+
+	client, err := mongo.Connect(context.Background(), opts) //connect to DB
+	if err != nil {
+		panic(err)
+	}
 }
